2463a2f97414909c4f9430fabbcccf79bf362100
tkkhunteta14 <70328933+tkkhunteta14@users.noreply.github.com>
2022-03-14 04:09:27 +0530

    Add files via upload

---
+++ Junit/TestingQ1/src/TestingQ1.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class TestingQ1 {
+    public static int[] findMinMax(int[] arr) {
+        int[] as = new int[2];
+        as[0] = (Arrays.stream(arr).min().getAsInt());
+        as[1] = (Arrays.stream(arr).max().getAsInt());
+        return as;
+    }
+  
+
+    public static String findMinMax2(int[] arr) {
+        int min = (Arrays.stream(arr).min().getAsInt());
+        int max = (Arrays.stream(arr).max().getAsInt());
+        return "Min is "+ min+" Max is "+max;
+    }
+}
